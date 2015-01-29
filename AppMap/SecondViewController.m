1fb2775531c1c6839bd3d56d0f1cf520f7f92d2a
Derik <derikflanary@gmail.com>
2015-01-29 15:36:03 -0700

    more

--- AppMap/SecondViewController.m
+++ AppMap/SecondViewController.m
@@ -8,7 +8,7 @@
 
 #import "SecondViewController.h"
 
-@interface SecondViewController ()<UITableViewDataSource, UITableViewDelegate>
+@interface SecondViewController ()
 
 
 
@@ -26,16 +26,5 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 2;
-}
-
-// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
-// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
-    return cell;
-}
 
 @end
